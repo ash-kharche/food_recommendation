02a2f3fca63d3e5f6f95ad2f7276e7e94644ef5e
Aishwarya Kharche <ash.kharche@gmail.com>
2018-09-27 21:31:08 +0530

    debugging heroku

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 app.use(routes);
-//app.set('port', (process.env.PORT || 3010));
-app.listen(process.env.PORT || 3010, function () {
-	var port = server.address().port;
-    console.log("Hey! welcome to food recommendation on " + port+ "port");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our Food Recommendation app is running on port ${ PORT }`);
 });
