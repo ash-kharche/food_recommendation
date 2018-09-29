4f3ea5403df5aef8a3aa0963be08afb907d7e432
Aishwarya Kharche <ash.kharche@gmail.com>
2018-09-29 14:51:56 +0530

    debugging

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,3 @@ router.put('/login', controllers.apiUser.login);
 router.put('/signUpUser', controllers.apiUser.signUpUser);
 
 module.exports = router;
-
-//heroku logs --tail --app food-recommendation
-
-//Pushing code via terminal to Heroku
-//git commit -m "Test: app not running"
-//git push heroku master
-
-//CREATE TABLE users(user_id serial PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL,password VARCHAR (50) NOT NULL,email VARCHAR (355) UNIQUE NOT NULL,created_on TIMESTAMP NOT NULL,last_login TIMESTAMP);
