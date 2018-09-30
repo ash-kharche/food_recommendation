ceb6b17df4d2bd830b6d9100d45a189585591cdf
Aishwarya Kharche <ash.kharche@gmail.com>
2018-09-30 23:23:49 +0530

    DB queries

--- controllers/apiUser.js
+++ controllers/apiUser.js
@@ -10,29 +10,39 @@ apiUser.test = function (req, res) {
 
 apiUser.signUpUser = function (req, res) {
   console.log("Username:  " +req.body.user_name)
-    models.users.create({
-        user_name: req.body.user_name,
-        email: req.body.email,
-        mobile_number: req.body.mobile_number,
-        password: req.body.password
-    }).then(function (err, results) {
-        if (err){
-          res.json(err);
-      } else {
-          res.json(results);
-      }
+
+db_pool.connect(function(err, client, done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+
+       
+  var query = "INSERT INTO users (user_name, email, mobile_number, password)  VALUES ($1, $2, $3, $4)";
+  client.query(query, [ req.body.user_name, req.body.email, req.body.mobile_number, req.body.password) ,function(err,result) {
+          //call `done()` to release the client back to the pool
+           done(); 
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result.rows);
+       });
     });
+
 }
 
 apiUser.login = function (req, res) {
-  console.log("######   LOGIN API   ######");
+  console.log("######   LOGIN API   ######" + req.body.mobile_number + "  " + req.body.password);
 
   db_pool.connect(function(err, client, done) {
        if(err){
            console.log("not able to get connection "+ err);
            res.status(400).send(err);
        } 
-       client.query("SELECT * FROM users where mobile_number = " + req.body.mobile_number + " AND password = "+ req.body.password ,function(err,result) {
+
+       var query = "SELECT * FROM users WHERE mobile_number = $1 AND password = $2";
+       client.query(query, [req.body.mobile_number, req.body.password] ,function(err,result) {
           //call `done()` to release the client back to the pool
            done(); 
            if(err){
