3f0d4ad0a3b78f5fe385f94a253daf140fa6b67a
Kharche <ash.kharche@gmail.com>
2019-01-21 18:41:19 +0530

    Bug fixing

--- controllers/apiQuestions.js
+++ controllers/apiQuestions.js
@@ -4,6 +4,6 @@ var db_pool = require('./../helpers/db');
 var apiQuestions = {};
 
 apiQuestions.submitAnswers = function (req, res) {
-  res.status(200).send({"Hello 123"});
+  res.status(200).send(Json.stringify("Hello 123"));
 }
 module.exports = apiQuestions;
