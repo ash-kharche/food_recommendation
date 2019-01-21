dc130c5302f7a5b39899d2adbb60fabc2da8bb7d
Kharche <ash.kharche@gmail.com>
2019-01-21 18:36:46 +0530

    Bug fixing

--- controllers/apiQuestions.js
+++ controllers/apiQuestions.js
@@ -3,23 +3,7 @@
 var db_pool = require('./../helpers/db');
 var apiQuestions = {};
 
-apiUser.submitAnswers = function (req, res) {
-db_pool.connect(function(err, client, done) {
-       if(err){
-           console.log("not able to get connection "+ err);
-           res.status(400).send(err);
-       }
-
-
-  var query = "INSERT INTO answers ()  VALUES ($1, $2, $3)";
-  client.query(query, [ req.body.date, req.body.total_amount, req.body.order_address] ,function(err,result) {
-           done();
-           if(err){
-               console.log(err);
-               res.status(400).send(err);
-           }
-           res.status(200).send(result.rows);
-       });
-    });
+apiQuestions.submitAnswers = function (req, res) {
+  res.status(200).send({"Hello 123"}});
 }
 module.exports = apiQuestions;
