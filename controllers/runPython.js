cdd20fa05c6fcfa1c618f672208995919866c77e
Kharche <ash.kharche@gmail.com>
2019-02-06 23:54:12 +0530

    Debugging Python

--- controllers/runPython.js
+++ controllers/runPython.js
@@ -3,28 +3,20 @@ var spawn = require("child_process").spawn;
 var runPython = {};
 
 //https://www.geeksforgeeks.org/run-python-script-node-js-using-child-process-spawn-method/
-/*runPython.test = function (req, res) {
-    var process = spawn('python',["./python/hello.py",
-                            req.query.firstname,
-                            req.query.lastname]);
-
-    process.stdout.on('data', function(err, data) {
-        if(err) {
-            console.log("Test: Err " + err.message);
-            res.send(err);
-        } else {
-            console.log("Test Success:  " + data.toString());
-            res.send({"message":"Hellow Poonam"});
-        }
-    })
-},*/
-
-runPython.test1 = function (req, res) {
-    var process = spawn('python',["./python/hello.py",
-                            req.query.firstname,
-                            req.query.lastname]);
+runPython.test = function (req, res) {
+    var process = spawn('python',["./python/hello.py"]);
 
     process.stdout.on('data', function(data) {
+          console.log("Test1 success:  " + data.toString());
+          res.send(data.toString());
+    })
+},
+
+runPython.test1 = function (req, res) {
+    var process = spawn('python',["./python/recommendation.py"]);
+
+    process.stdout.on('data', function(data) {
+          //console.log('data', new Buffer(data,'utf-8').toString(););
           console.log("Test2 success:  " + data.toString());
           res.send(data.toString());
     })
