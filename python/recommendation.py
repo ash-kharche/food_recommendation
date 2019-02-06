aead37e4e9c9c3d0d73d184a44bb1060d66fa090
Kharche <ash.kharche@gmail.com>
2019-02-07 00:09:14 +0530

    Debugging Python

--- python/recommendation.py
+++ python/recommendation.py
@@ -3,22 +3,26 @@ import sys
 import psycopg2
 
 print ("Trying to connect to database...")
+sys.stdout.flush()
 
 conn = psycopg2.connect(database = "de7pit5nq8p35l", user = "uvzjkvhjhvrevn", password = "e9fa122e883f4af209c53c4586f1e7d2e38fee3495a03873c0191b98cea73fb2", host = "ec2-54-243-223-245.compute-1.amazonaws.com", port = "5432")
 print ("Opened database successfully")
+sys.stdout.flush()
 
 cur = conn.cursor()
 # execute a statement
 print('PostgreSQL database version:')
+sys.stdout.flush()
+
 cur.execute('SELECT version()')
 
 # display the PostgreSQL database server version
 db_version = cur.fetchone()
 print(db_version)
+sys.stdout.flush()
 
 # close the communication with the PostgreSQL
 cur.close()
 
 conn.commit()
 conn.close()
-sys.stdout.flush()
