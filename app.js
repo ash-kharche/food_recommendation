f34ba3bada75fbf6675e1fe972c7f23581d8799a
Kharche <ash.kharche@gmail.com>
2019-02-06 16:19:42 +0530

    Server not working debugging

--- app.js
+++ app.js
@@ -7,48 +7,6 @@ app.use(bodyParser.json());
 app.use(routes);
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function() {
-    console.log('server running on port ' + PORT);
-} )
-
-// Function callName() is executed whenever
-// url is of the form localhost:3000/name
-app.get('/test', callName);
-
-function callName(req, res) {
-
-    // Use child_process.spawn method from
-    // child_process module and assign it
-    // to variable spawn
-    var spawn = require("child_process").spawn;
-
-    // Parameters passed in spawn -
-    // 1. type_of_script
-    // 2. list containing Path of the script
-    //    and arguments for the script
-
-    // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will
-    // so, first name = Mike and last name = Will
-    var process = spawn('python',["./hello.py",
-                            req.query.firstname,
-                            req.query.lastname] );
-
-    // Takes stdout data from script which executed
-    // with arguments and send this data to res object
-    process.stdout.on('data', function(data) {
-        var textChunk = data.toString('utf8');
-        res.send(textChunk);
-    } )
-}
-
-/*app.listen(PORT, function (err, client) {
-    if(err) {
-        console.log("Our Food Recommendation app has ERROR:  " +err.message);
-    } else {
-        console.log("Our Food Recommendation app is running on port ${"+ PORT +"}");
-    }
-});
-/*
 var db_pool = require('./helpers/db');
 
 db_pool.connect(function (err, client) {
@@ -66,4 +24,3 @@ db_pool.connect(function (err, client) {
     }
 
 });
-*/
