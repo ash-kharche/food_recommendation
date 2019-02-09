c070f8ba76dcfdc97edc13d858be150dd49abece
Kharche <ash.kharche@gmail.com>
2019-02-09 15:48:49 +0530

    Trending products python

--- python/trending_products.py
+++ python/trending_products.py
@@ -5,7 +5,9 @@ import psycopg2
 conn = psycopg2.connect(database = "de7pit5nq8p35l", user = "uvzjkvhjhvrevn", password = "e9fa122e883f4af209c53c4586f1e7d2e38fee3495a03873c0191b98cea73fb2", host = "ec2-54-243-223-245.compute-1.amazonaws.com", port = "5432")
 cursor = conn.cursor()
 
-postgreSQL_select_Query = "SELECT * FROM products ORDER BY rating DESC LIMIT 5"
+#postgreSQL_select_Query = "SELECT * FROM products ORDER BY rating DESC LIMIT 5"
+postgreSQL_select_Query = "SELECT product_id, product_name, price, rating FROM products ORDER BY rating DESC LIMIT 5"
+
 cursor.execute(postgreSQL_select_Query)
 
 trending_products = cursor.fetchall()
@@ -15,7 +17,15 @@ conn.commit()
 conn.close()
 
 #https://medium.com/@PyGuyCharles/python-sql-to-json-and-beyond-3e3a36d32853
-trending_products = [dict(zip(column, row)) for row in result];
-return json.dumps('trending_products' : trending_products)
+#https://www.peterbe.com/plog/from-postgres-to-json-strings
+
+columns = ('product_id', 'product_name', 'price', 'rating')
+
+results = []
+for row in trending_products:
+        results.append(dict(zip(columns, row)))
+
+print(json.dumps(results, indent=2))
+
 #print(trending_products)
 #sys.stdout.flush()
