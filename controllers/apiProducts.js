61d638cc86cfc572dbae0088e5385c4ecd561df3
Kharche <ash.kharche@gmail.com>
2019-02-12 00:43:38 +0530

    getData Api

--- controllers/apiProducts.js
+++ controllers/apiProducts.js
@@ -11,14 +11,28 @@ apiProducts.getData = function (req, res) {
             res.status(400).send(err);
 
         } else {
-           console.log("$$$$$$$$$$$$$$$$$$$$$$$");
+            console.log("$$$$$$$$$$$$$$$$$$$$$$$");
 
             var queryCollections = "SELECT * FROM collections";
             var queryProducts = "SELECT * FROM products";
-            var queryTrendingProducts = "SELECT * FROM trending_products";
-            var queryRecommendedProducts = "SELECT * FROM recommended_products";
 
             var data = {};
+
+
+            async.parallel({
+                one: apiProducts.getCollectionsDB(callback) {
+                    //callback(null, 'abc\n');
+                },
+                two: apiProducts.getProducts(callback) {
+                    //callback(null, 'xyz\n');
+                }
+            }, function (err, results) {
+                // results now equals to: results.one: 'abc\n', results.two: 'xyz\n'
+
+                console.log("ApiProducts : async parallel    :" + results);
+            });
+
+            /*
             Promise.all([
                 client.query(queryCollections,function(err, result) {
                         done();
@@ -49,28 +63,27 @@ apiProducts.getData = function (req, res) {
                             data.trending_products = JSON.parse(response);
                             //res.status(200).send(data);
                         }
-                    })
-                    /*,
+                    }),
                     runPython.getRecommendedProducts(function(err, response) {
                       if(err) {
                           console.log("ApiProducts : recommended_products    :" + err);
-                          data.recommended_products = {};
+                          data.recommended_products = [];
                       } else {
                          console.log("ApiProducts : recommended_products    :" + response);
-                         data.recommended_products = [];
                          data.recommended_products = JSON.stringify(rresponse);
                        }
                     })
-                    */
+
               ])
             .then(function() {
-                  console.log("\n@@@@@\nApiProducts response send");
+                  console.log("\n@@@@@\ApiProducts response send");
                   res.status(200).send(data);
             }).catch(err => {
+                  console.log("\n@@@@@\ApiProducts Error\n\n");
                   console.error(err);
                   res.status(400).send(err);
             });
-
+            */
         }
     });
 },
@@ -99,7 +112,48 @@ apiProducts.getData = function (req, res) {
                 }
             });
         });
+    },
+
+    apiProducts.getProducts = function (callback) {
+        console.log("\n apiProducts: getProducts");
+        db_pool.connect(function (err, client, done) {
+            if (err) {
+                callback(err, null);
+            } else {
+                var query = "SELECT * FROM products";
+                client.query(query, function (err, result) {
+                    done();
+                    if (err) {
+                        console.log(err);
+                        callback(err, null);
+
+                    } else {
+                        callback(null, result.rows);
+                    }
+                });
+            }
+        });
+    },
+    apiProducts.getCollectionsDB = function (callback) {
+        console.log("\n apiProducts: getCollectionsDB");
+        db_pool.connect(function (err, client, done) {
+            if (err) {
+                callback(err, null);
+            } else {
+                var query = "SELECT * FROM collections";
+                client.query(query, function (err, result) {
+                    done();
+                    if (err) {
+                        console.log(err);
+                        callback(err, null);
+
+                    } else {
+                        callback(null, result.rows);
+                    }
+                });
+            }
+        });
     }
 
 
-module.exports = apiProducts;
+    module.exports = apiProducts;
