08c731483bda80d08443b38cdd88d88b79df7a0c
Kharche <ash.kharche@gmail.com>
2019-02-11 18:01:14 +0530

    Debugging Trending products api

--- python/trending_products.py
+++ python/trending_products.py
@@ -6,13 +6,14 @@ import json
 
 conn = psycopg2.connect(database = "de7pit5nq8p35l", user = "uvzjkvhjhvrevn", password = "e9fa122e883f4af209c53c4586f1e7d2e38fee3495a03873c0191b98cea73fb2", host = "ec2-54-243-223-245.compute-1.amazonaws.com", port = "5432")
 cursor = conn.cursor(cursor_factory=RealDictCursor)
+
 postgreSQL_select_Query = "SELECT * FROM collections"
-rows = cursor.execute(postgreSQL_select_Query).fetchall()
+cursor.execute(postgreSQL_select_Query)
+rows = cursor.fetchall()
 
 cursor.close()
 conn.commit()
 conn.close()
 
-#rows = {"name":"dombivli"}
 print(json.dumps(rows, indent=2))
 sys.stdout.flush()
