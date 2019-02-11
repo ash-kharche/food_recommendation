b2d57ae664f50ba8d11b45fd36061bebfa5bd5e2
Kharche <ash.kharche@gmail.com>
2019-02-12 00:15:44 +0530

    Debugging Trending products api

--- python/trending_products.py
+++ python/trending_products.py
@@ -9,7 +9,7 @@ conn = psycopg2.connect(database = "de7pit5nq8p35l", user = "uvzjkvhjhvrevn", pa
 cursor = conn.cursor(cursor_factory=RealDictCursor)
 
 #postgreSQL_select_Query = "SELECT * FROM products ORDER BY rating LIMIT 5"
-postgreSQL_select_Query = "SELECT product_name, rating FROM products"
+postgreSQL_select_Query = "SELECT * FROM products"
 cursor.execute(postgreSQL_select_Query)
 rows = cursor.fetchall()
 
