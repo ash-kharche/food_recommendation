43b2aa5c2fc9f10377590ce091a852e08e73903a
Kharche <ash.kharche@gmail.com>
2019-03-13 01:10:11 +0530

    Debugging

--- controllers/apiProducts.js
+++ controllers/apiProducts.js
@@ -43,31 +43,31 @@ apiProducts.getData = function (req, res) {
             });
 
             var getTrendingProductsPromise = new Promise(function (resolve, reject) {
-              apiProducts.getTrendingProducts(function (err, response) {
-                  if (err) {
-                      //console.log("ApiProducts : trending_products    :" + err);
-                      data.trending_products = [];
-                      return reject();
-                  } else {
-                      //console.log("ApiProducts : trending_products    :" + response);
-                      data.trending_products = response;
-                      return resolve(response);
-                  }
-              });
+                apiProducts.getTrendingProducts(function (err, response) {
+                    if (err) {
+                        //console.log("ApiProducts : trending_products    :" + err);
+                        data.trending_products = [];
+                        return reject();
+                    } else {
+                        //console.log("ApiProducts : trending_products    :" + response);
+                        data.trending_products = response;
+                        return resolve(response);
+                    }
+                });
             });
 
             var getRecommendedProductsPromise = new Promise(function (resolve, reject) {
-              apiProducts.getTrendingProducts(function (err, response) {
-                  if (err) {
-                      //console.log("ApiProducts : trending_products    :" + err);
-                      data.recommended_products = [];
-                      return reject();
-                  } else {
-                      //console.log("ApiProducts : trending_products    :" + response);
-                      data.recommended_products = response;
-                      return resolve(response);
-                  }
-              });
+                apiProducts.getRecommendedProducts(function (err, response) {
+                    if (err) {
+                        //console.log("ApiProducts : trending_products    :" + err);
+                        data.recommended_products = [];
+                        return reject();
+                    } else {
+                        //console.log("ApiProducts : trending_products    :" + response);
+                        data.recommended_products = response;
+                        return resolve(response);
+                    }
+                });
             });
 
             Promise.all([
@@ -156,6 +156,61 @@ apiProducts.getData = function (req, res) {
         });
     },
 
+    apiProducts.getRecommendedProducts = function (userId, callback) {
+        //console.log("\n apiProducts: getProducts");
+        db_pool.connect(function (err, client, done) {
+            if (err) {
+                callback(err, null);
+            } else {
+                var query = "SELECT * FROM orders WHERE user_id = " + userId;
+                client.query(query, function (err, result) {
+                    //done();
+                    if (err) {
+                        console.log(err);
+                        callback(err, null);
+
+                    } else {
+
+                        var ingredientsIdList = [];
+                        var myString = "";
+                        for (var i = 0; i < result.rows.length; i++) {
+                            var order = result.rows[i];
+                            for (var k = 0; k < order.products.length; k++) {
+                                //console.log("\n&&&&&&&&&&&&&&&\t product_id:   " + order.products[k].product_id);
+                                ingredientsIdList.push(order.products[k].ingredients);
+                                myString = myString + "," + order.products[k].ingredients;
+                            }
+                        }
+                        console.log("\n********* myString:   " + myString);
+
+                        var productsArray = [];
+                        var query = "SELECT * FROM products WHERE ingredients && ARRAY[" + ingredientsIdList + "]";
+                        console.log("query:   " + query);
+                        client.query(query, function (err, result) {
+                            console.log("111");
+                            //done();
+
+                            if (err) {
+                                console.log("222");
+                                console.log(err);
+                                callback(new Error("No products found matching ingredients"), null);
+
+                            } else {
+                                console.log("333");
+                                console.log("\n*********t products" + result.rows);
+                                productsArray.push(result.rows); //TODO check if multiple products available
+                            }
+
+
+                        });
+
+                        callback(null, result.rows);
+                    }
+                });
+            }
+        });
+    },
+
     apiProducts.getCollectionsDB = function (callback) {
         //console.log("\n apiProducts: getCollectionsDB");
         db_pool.connect(function (err, client, done) {
@@ -178,7 +233,7 @@ apiProducts.getData = function (req, res) {
     },
 
     apiProducts.getProductById = function (productId, callback) {
-        console.log("\n apiProducts: getProductById::   " +productId);
+        console.log("\n apiProducts: getProductById::   " + productId);
         db_pool.connect(function (err, client, done) {
             if (err) {
                 callback(err, null);
