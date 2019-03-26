07ca8dc2b83f99b87f877b9fe5f9064824d33c3c
Kharche <ash.kharche@gmail.com>
2019-03-26 15:16:18 +0530

    Python: predictRating

--- python/hybrid.py
+++ python/hybrid.py
@@ -402,12 +402,12 @@ def predictRating(data, user_data, item_data):
 
 ratings_csv_data = readingFile(file1)
 print("ratings_csv_data")
-#print(ratings_csv_data)
-user_data = userData(file2)
-print("user_data")
+print(ratings_csv_data)
+#user_data = userData(file2)
+#print("user_data")
 #print(user_data)
-item_data = itemData(file3)
-print("item_data")
+#item_data = itemData(file3)
+#print("item_data")
 #print(item_data)
 
 #sim_item_cosine, sim_item_jaccard, sim_item_pearson = similarity_item(item_data)
