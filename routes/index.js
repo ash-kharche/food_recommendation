98c9bcbbcf5b3c65f858cf29e7fd3871f1b8bb80
Kharche <ash.kharche@gmail.com>
2019-04-20 22:43:24 +0530

    New data added: testing routes

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ var controllers = require("../controllers");
 
 //router.get('/test', controllers.runPython.test);
 router.get('/getTrendingProducts', controllers.runPython.getTrendingProducts);
-router.get('/getUserRecommendedProducts', controllers.apiProducts.getUserRecommendedProducts);
+router.post('/getUserRecommendedProducts', controllers.apiProducts.getUserRecommendedProducts);
 router.get('/getUserRecommendedProducts_1/:user_id', controllers.apiRecommendation.getUserRecommendedProducts);
-router.get('/getCartRecommendedProducts', controllers.apiProducts.getCartRecommendedProducts);
+router.post('/getCartRecommendedProducts', controllers.apiProducts.getCartRecommendedProducts);
 router.get('/calculateNutrients', controllers.apiProducts.calculateNutrients_otherway);
 
 router.put('/login', controllers.apiUser.login);
@@ -15,7 +15,7 @@ router.put('/signUpUser', controllers.apiUser.signUpUser);
 router.get('/getAllUsers', controllers.apiUser.getAllUsers);
 router.get('/getUser/:user_id', controllers.apiUser.getUser);
 
-router.get('/getData', controllers.apiProducts.getData);
+router.post('/getData', controllers.apiProducts.getData);
 router.get('/getCollections', controllers.apiProducts.getCollections);
 //router.get('/getTrendingProducts', controllers.apiProducts.getTrendingProducts);
 
